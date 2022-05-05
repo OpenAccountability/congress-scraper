6cb406750c99485620ecdcdb5d2b6213dea375de
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-05 18:52:39 -0500

    Add helper methods to extract a result set of data from a response

--- congress/members.py
+++ congress/members.py
@@ -1,6 +1,28 @@
 from argparse import Namespace
 
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet
+from requests import get
+from requests.models import Response
+
 from congress.args import memberArgs
+
+def getRequest(url: str)    -> Response:
+    return get(url)
+
+def buildSoup(resp: Response)   ->  BeautifulSoup:
+    return BeautifulSoup(markup=resp.content, features="lxml")
+
+def getElements(soup: BeautifulSoup)    ->  ResultSet:
+    return soup.find_all(name="li", attrs={"class": "expanded"})
+
+def extractMembers(data: ResultSet, df)
 
 def main()  ->  None:
     args: Namespace = memberArgs()
+
+    resp: Response = getRequest('https://www.congress.gov/search?q={%22source%22:%22members%22}&pageSize=250')
+
+    soup: BeautifulSoup = buildSoup(resp)
+
+    print(getElements(soup))

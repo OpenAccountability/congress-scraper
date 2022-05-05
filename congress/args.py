0ace856a972673f5fa4fc9b14d1973c92c199871
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-05 18:24:50 -0500

    Add the arguement handling

---
+++ congress/args.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser, Namespace
+from congress.version import version
+
+authors: list = ["Nicholas M. Synovic"]
+name: str = "Congress"
+
+
+def memberArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog=f"{name} Member Scraper",
+        usage="A tool to download the metadata for all Congress Members listed on https://congress.gov",
+        description="This tool downloads all of the front matter for Congress Members listed on https://congress.gov",
+        epilog=f"Created by: {', '.join(authors)}",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        required=False,
+        type=bool,
+        default=False,
+        action="version",
+        version=f"{version()}"
+        help="Display the version",
+    )
+
+    return parser.parse_args()

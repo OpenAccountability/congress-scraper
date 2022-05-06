639a6254cb4a3d2ceacd0bdce39ef5c29505c256
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-06 09:10:52 -0500

    Add more description to the code

--- cd-bills.bash
+++ cd-bills.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "This script only downloads the top 1000 bills from each Senator or Representative. It is not a complete dataset."
+
 membersFile=$1
 mkdir bills/
 
-jq -r .[].Key $membersFile | parallel --bar wget -O bills/{}.csv 'https://www.congress.gov/quick-search/legislation?wordsPhrases=\&wordVariants=on\&congressGroups%5B%5D=0\&congressGroups%5B%5D=1\&congresses%5B%5D=all\&legislationNumbers=\&legislativeAction=\&sponsor=on\&representative={}\&senator={}\&1ddcb92ade31c8fbd370001f9b29a7d9=628cb5675ff524f3e719b7aa2e88fe3f'
+jq -r .[].Key $membersFile | parallel --bar wget -q -O bills/{}.csv 'https://www.congress.gov/quick-search/legislation?wordsPhrases=\&wordVariants=on\&congressGroups%5B%5D=0\&congressGroups%5B%5D=1\&congresses%5B%5D=all\&legislationNumbers=\&legislativeAction=\&sponsor=on\&representative={}\&senator={}\&1ddcb92ade31c8fbd370001f9b29a7d9=628cb5675ff524f3e719b7aa2e88fe3f'

0b7e77efeaa8ef5a02250c4f052cb303d89515f2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-05 19:48:36 -0500

    Ran black and isort

--- congress/members.py
+++ congress/members.py
@@ -1,17 +1,16 @@
 from argparse import Namespace
 from distutils.command.build import build
+from re import findall
 
+import pandas
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet, Tag
 from pandas import DataFrame
 from requests import get
 from requests.models import Response
 
-from re import findall
-
 from congress.args import memberArgs
 
-import pandas
 
 def getRequest(url: str) -> Response:
     return get(url)
@@ -20,10 +19,14 @@ def getRequest(url: str) -> Response:
 def buildSoup(resp: Response) -> BeautifulSoup:
     return BeautifulSoup(markup=resp.content, features="lxml")
 
-def getPageCount(soup: BeautifulSoup)   ->  int:
+
+def getPageCount(soup: BeautifulSoup) -> int:
     pagination: Tag = soup.find(name="div", attrs={"class": "pagination"})
-    pageCountText: str = pagination.findChild(name="span", attrs={"class": "results-number"}).text
+    pageCountText: str = pagination.findChild(
+        name="span", attrs={"class": "results-number"}
+    ).text
     return int(findall("\d+", pageCountText)[0])
+
 
 def getElements(soup: BeautifulSoup) -> ResultSet:
     return soup.find_all(name="li", attrs={"class": "expanded"})
@@ -67,7 +70,8 @@ def main() -> None:
             dfList.append(extractMembers(dataset=data))
         else:
             resp: Response = getRequest(
-                "https://www.congress.gov/search?q={%22source%22:%22members%22}&pageSize=250&page=" + str(page)
+                "https://www.congress.gov/search?q={%22source%22:%22members%22}&pageSize=250&page="
+                + str(page)
             )
             soup: BeautifulSoup = buildSoup(resp)
             data: ResultSet = getElements(soup)

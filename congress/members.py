03dbae616a172c2629a7d6c4b1d3fe7687451b64
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-05 19:29:52 -0500

    Add code to extract data from a webpage and store it in a dataframe

--- congress/members.py
+++ congress/members.py
@@ -1,28 +1,57 @@
 from argparse import Namespace
 
 from bs4 import BeautifulSoup
-from bs4.element import ResultSet
+from bs4.element import ResultSet, Tag
+from pandas import DataFrame
 from requests import get
 from requests.models import Response
 
 from congress.args import memberArgs
 
-def getRequest(url: str)    -> Response:
+
+def getRequest(url: str) -> Response:
     return get(url)
 
-def buildSoup(resp: Response)   ->  BeautifulSoup:
+
+def buildSoup(resp: Response) -> BeautifulSoup:
     return BeautifulSoup(markup=resp.content, features="lxml")
 
-def getElements(soup: BeautifulSoup)    ->  ResultSet:
+
+def getElements(soup: BeautifulSoup) -> ResultSet:
     return soup.find_all(name="li", attrs={"class": "expanded"})
 
-def extractMembers(data: ResultSet, df)
 
-def main()  ->  None:
+def extractMembers(dataset: ResultSet) -> DataFrame:
+    data: dict = {"Last Name": [], "First Name": [], "URL": []}
+
+    tag: Tag
+    for tag in dataset:
+        element: Tag = tag.findChild(name="span", attrs={"class": "result-heading"})
+
+        title: str = element.text
+        title.replace(",", "")
+        splitTitle: list = title.split(" ")
+
+        data["Last Name"].append(splitTitle[1])
+        data["First Name"].append(splitTitle[2])
+        data["URL"].append("https://congress.gov" + element.findChild("a").get("href"))
+
+    return DataFrame(data)
+
+
+def main() -> None:
     args: Namespace = memberArgs()
 
-    resp: Response = getRequest('https://www.congress.gov/search?q={%22source%22:%22members%22}&pageSize=250')
+    columns: list = ["Last Name", "First Name", "URL"]
+
+    df: DataFrame = DataFrame(columns=columns)
+
+    resp: Response = getRequest(
+        "https://www.congress.gov/search?q={%22source%22:%22members%22}&pageSize=250"
+    )
 
     soup: BeautifulSoup = buildSoup(resp)
 
-    print(getElements(soup))
+    data: ResultSet = getElements(soup)
+
+    extractMembers(data, df)
